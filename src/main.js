f2242d0c7af63b049ed2752713782595be37d47a
perfectionist007 <turash.chowdhury@gmail.com>
2025-03-10 20:53:12 +0600

    second commit

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import kaplay from "kaplay";
 const k = kaplay();
 
 k.loadRoot("./"); // A good idea for Itch.io publishing later
-k.loadSprite("bean", "sprites/pizza.png");
+k.loadSprite("bean", "sprites/bean.png");
 
 k.add([k.pos(120, 80), k.sprite("bean")]);
 
